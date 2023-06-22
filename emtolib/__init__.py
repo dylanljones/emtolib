2e34a7e3ab845f1c93b94195c1fa33a3ffed32b6
dylanljones <dylanljones94@gmail.com>
2023-06-22 19:47:45 +0200

    refactor: improve imports

--- emtolib/__init__.py
+++ emtolib/__init__.py
@@ -5,7 +5,9 @@
 # Copyright (c) 2022, Dylan Jones
 
 from .common import logger, elements
-from . import input_files
-from . import output_files
-from .directory import EmtoDirectory, walk_emtodirs
+
+from .input_files import EmtoKgrnFile, Atom, KGRNError, EmtoBmdlFile
+from .output_files import EmtoPrnFile, EmtoDosFile
 from .slurm import SlurmScript
+
+from .directory import EmtoDirectory, walk_emtodirs
