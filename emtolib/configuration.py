67ba6848bbd1b23da9f975cd499cb000b28fa0a4
dylanljones <dylanljones94@gmail.com>
2023-07-03 15:56:14 +0200

    rename args

--- emtolib/configuration.py
+++ emtolib/configuration.py
@@ -12,10 +12,10 @@ from .files import EmtoKgrnFile
 def read_config(file="emto.ini"):
     parser = ConfigParser()
     parser.read(file)
-    config = dict()
+    conf = dict()
     emto = parser["emto"]
     root = Path(emto["root"])
-    config["emto"] = dict(
+    conf["emto"] = dict(
         root=root,
         executable=str(root / emto["executable"]).replace("\\", "/"),
         executable1=str(root / emto["executable1"]).replace("\\", "/"),
@@ -24,31 +24,31 @@ def read_config(file="emto.ini"):
         kstr=str(root / emto["kstr"]).replace("\\", "/"),
         bmdl=str(root / emto["bmdl"]).replace("\\", "/"),
     )
-    config["slurm"] = dict(parser["slurm"])
-    config.update(**parser["general"])
+    conf["slurm"] = dict(parser["slurm"])
+    conf.update(**parser["general"])
 
-    return config
+    return conf
 
 
-def update_slurm_settings(slurm, config, executable, jobname):
-    slurm.ntasks = config["slurm"]["ntasks"]
-    slurm.nodes = config["slurm"]["nodes"]
-    slurm.mail_user = config["slurm"]["mail_user"]
-    slurm.mail_type = config["slurm"]["mail_type"]
-    slurm.mem = config["slurm"]["mem"]
-    slurm.time = config["slurm"]["time"]
+def update_slurm_settings(slurm, conf, executable, jobname):
+    slurm.ntasks = conf["slurm"]["ntasks"]
+    slurm.nodes = conf["slurm"]["nodes"]
+    slurm.mail_user = conf["slurm"]["mail_user"]
+    slurm.mail_type = conf["slurm"]["mail_type"]
+    slurm.mem = conf["slurm"]["mem"]
+    slurm.time = conf["slurm"]["time"]
 
     executable = executable.replace("\\", "/")
     i, _ = slurm.find_command("time")
     slurm.commands[i] = f"time {executable} < {jobname}.dat"
 
 
-def update_emto_paths(dat: EmtoKgrnFile, config, kstr, bmdl, kstr2=""):
-    kstr_path = config["emto"]["kstr"] + "/" + kstr
+def update_emto_paths(dat: EmtoKgrnFile, conf, kstr, bmdl, kstr2=""):
+    kstr_path = conf["emto"]["kstr"] + "/" + kstr
     if not kstr2:
         kstr2 = kstr
-    kstr2_path = config["emto"]["kstr"] + "/" + kstr2
-    bmdl_path = config["emto"]["bmdl"] + "/" + bmdl
+    kstr2_path = conf["emto"]["kstr"] + "/" + kstr2
+    bmdl_path = conf["emto"]["bmdl"] + "/" + bmdl
 
     dat.for001 = kstr_path
     dat.for001_2 = kstr2_path
