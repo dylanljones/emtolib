c615cca7faae850d40114787f508dc06316c2341
dylanljones <dylanljones94@gmail.com>
2023-07-26 11:48:27 +0200

    improve __main__

--- emtolib/__main__.py
+++ emtolib/__main__.py
@@ -4,6 +4,7 @@
 
 import sys
 from argparse import ArgumentParser
+from .files import KGRNError
 from .directory import walk_emtodirs, is_emtodir, EmtoDirectory
 
 
@@ -13,27 +14,29 @@ def iter_emtodirs(args):
     except AttributeError:
         recursive = True
     for argpath in args.paths:
-        if is_emtodir(argpath):
-            yield EmtoDirectory(argpath)
+        try:
+            if is_emtodir(argpath):
+                yield EmtoDirectory(argpath)
+        except KGRNError as e:
+            print(e)
+
         for folder in walk_emtodirs(argpath, recursive=recursive):
             yield folder
 
 
 def add_path_arg(parser):
     helpstr = "Path to one or more EMTO directories"
-    parser.add_argument("paths", type=str, nargs="+", help=helpstr)
+    parser.add_argument("paths", type=str, nargs="*", default=[""], help=helpstr)
 
 
 def init_argparser():
     parser = ArgumentParser("emtolib")
-    parser.add_argument(
-        "--verbose", "-v", default=False, action="store_true", help="Verbose output"
-    )
     subparsers = parser.add_subparsers(dest="command")
 
     # Grep command
     parser_grep = subparsers.add_parser("grep", help="Grep emto directories")
     parser_grep.add_argument("pattern", type=str, help="Pattern to grep for")
+    parser_grep.add_argument("-t", "--type", type=str, help="File types")
     add_path_arg(parser_grep)
 
     # Converged command
@@ -45,9 +48,14 @@ def init_argparser():
     add_path_arg(parser_iter)
 
     # Set command
-    parser_set = subparsers.add_parser("set", help="Set emto directory")
+    parser_set = subparsers.add_parser("set", help="Set variable of EMTO input file")
     parser_set.add_argument("value", type=str, nargs=1, help="Value to set (key=value)")
     add_path_arg(parser_set)
+
+    # Set command
+    parser_get = subparsers.add_parser("get", help="Get variable of EMTO input file")
+    parser_get.add_argument("key", type=str, nargs=1, help="Key to get")
+    add_path_arg(parser_get)
 
     # Check DOS command
     parser_set = subparsers.add_parser("check_dos", help="Check DOS output")
@@ -89,10 +97,16 @@ def handle_set(args):
     value = value.strip()
     for folder in iter_emtodirs(args):
         dat = folder.dat
-        if args.verbose:
-            print(f"Setting {key} to {value}: {folder.path}")
+        print(f"{folder.path}: Setting {key} to {value}")
         dat[key] = value
         dat.dump()
+
+
+def handle_get(args):
+    key = args.key[0]
+    for folder in iter_emtodirs(args):
+        dat = folder.dat
+        print(f"{folder.path}: {dat[key]}")
 
 
 def handle_check_dos(args):
@@ -116,6 +130,7 @@ HANDLERS = {
     "converged": handle_converged,
     "iter": handle_iter,
     "set": handle_set,
+    "get": handle_get,
     "check_dos": handle_check_dos,
 }
 
